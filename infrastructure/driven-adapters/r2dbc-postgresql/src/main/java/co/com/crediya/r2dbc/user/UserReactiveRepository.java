c080e8ec550f1f8bbdd2944355f05f50ccf4f66c
JuanesOsorioL <juanchis_1989@hotmail.com>
2025-09-08 16:29:45 -0500

    se inicio con la HU 04 de listar solicitud

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/user/UserReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/user/UserReactiveRepository.java
@@ -4,12 +4,17 @@ import co.com.crediya.model.user.User;
 import co.com.crediya.r2dbc.entities.UserEntity;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Collection;
 
 public interface UserReactiveRepository extends ReactiveCrudRepository<UserEntity, String>, ReactiveQueryByExampleExecutor<UserEntity> {
 
     Mono<Boolean> existsByEmail(String email);
     Mono<User> findByDocumentId(String documentId);
+    Flux<User> findByDocumentIdIn(Collection<String> documentIds);
     Mono<Boolean> existsByDocumentId(String documentId);
     Mono<User> findByEmailAndPassword(String email, String password);
 }
+
